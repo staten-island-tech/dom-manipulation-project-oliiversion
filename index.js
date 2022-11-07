f61c41d41ef0214267311e15686515d49ef5420d
oliiversion <olivxrkei101@gmail.com>
2022-11-07 13:15:48 -0500

    ye

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const DOMSelectors = {
   button: document.getElementById("button"),
-  text: document.querySelector("text"),
-  point: document.querySelectorAll(".point"),
-  pointTwo: doc.getElementById("pointTwo"),
+  text: document.querySelector("#text"),
+  box: document.getElementById("big-box"),
+  points: document.querySelectorAll(".point"),
 };
-console.log(DOMSelectors.pointTwo);
 
-function backgroundAndText(backgroud, text) {
-  backgroud.style.backgroundColor = "red";
-  text.innerHTML = : "big red box";
-  
+function backgroundAndText(background, text) {
+  background.style.backgroundColor = "red";
+  text.innerHTML = "big red box";
+  text.style.fontSize = "40px";
+}
+
+DOMSelectors.button.addEventListener("click", function () {
+  backgroundAndText(DOMSelectors.box, DOMSelectors.text);
+});
+function changeLi() {
+  DOMSelectors.points.forEach((point) => {
+    console.log(points);
+  });
 }
