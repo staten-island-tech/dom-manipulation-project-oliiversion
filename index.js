4f643da5febbbd5e4246a8f633b66aa70e46d508
oliiversion <olivxrkei101@gmail.com>
2022-11-17 13:12:01 -0500

    yo

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
-DOMSelectors = {
-  submit: document.getElementById("submit"),
+const DOMSelectors = {
+  put: document.getElementById("put"),
   name: document.getElementById("name"),
   title: document.getElementById("title"),
   image: document.getElementById("image"),
   stuff: document.getElementById("stuff"),
 };
-DOMSelectors.submit.addEventListener("click", function () {
-  p = DOMSelectors.name.value;
-  h2 = DOMSelectors.title.value;
-  image = DOMSelectors.image.value;
+DOMSelectors.put.addEventListener("click", function () {
+  let p = DOMSelectors.name.value;
+  let h2 = DOMSelectors.title.value;
+  let image = DOMSelectors.image.value;
+
   DOMSelectors.stuff.insertAdjacentHTML(
-    "afterbegin",
+    "",
     `<div class="stuff">
-              <h2>${h2}</h2>
+             <h2>${h2}</h2>
               <p>${p}</p>
               <img class="stuff-image" src="${image}">
               <br>
-              <button class="remove-btn" onclick="this.parentElement.remove()">Remove</button>
+
+            
+              /* <button class="remove-btn" onclick="this.parentElement.remove()">delete</button>
+              cannot be onclick button!!! */
       </div>`
   );
   DOMSelectors.name.value = "";
