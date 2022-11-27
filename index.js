dcefcd0a8b795fcbad20ddc6295912a42fd1d959
oliiversion <olivxrkei101@gmail.com>
2022-11-27 14:23:55 -0500

    tbh idk what else to do ive redone this too many times

--- index.js
+++ index.js
@@ -1,46 +1,39 @@
-DOMSelectors = {
-  submit: document.getElementById("submit"),
-};
 const DOMSelectors = {
-  put: document.getElementById("put"),
-  name: document.getElementById("name"),
-  title: document.getElementById("title"),
-  image: document.getElementById("image"),
-  stuff: document.getElementById("stuff"),
-  remove: document.getElementById("remove"),
+  song: document.getElementById("song"),
+  artist: document.getElementById("artist"),
+  imgURL: document.getElementById("imgURL"),
+  create: document.getElementById("create"),
+  box: document.getElementById("box"),
+  display: document.querySelector("display"),
 };
-DOMSelectors.put.addEventListener("click", function () {
-  let p = DOMSelectors.name.value;
-  let h2 = DOMSelectors.title.value;
-  let image = DOMSelectors.image.value;
 
-  DOMSelectors.stuff.insertAdjacentHTML(
-    "",
-    `<div class="stuff">
-             <h2>${h2}</h2>
-              <p>${p}</p>
-              <img class="stuff-image" src="${image}">
-              <br>
+DOMSelectors.create.addEventListener("click", function (event) {
+  event.preventDefault();
+  let input1 = DOMSelectors.song.value;
+  let input2 = DOMSelectors.artist.value;
+  let input3 = DOMSelectors.imgURL.value;
 
-            
+  function makeCard() {
+    DOMSelectors.box.insertAdjacentHTML(
+      "afterbegin",
+      `<div class="display">
+          <image src></image>
+          <h1 class="song">Title: ${input1}</h1>
+          <img src="${input3}" class="myImage"alt="">
+        <h2 class="artist">Song By:${input2}</h2>
+        <button class="remove">Delete</button>
       </div>`
-  );
-  DOMSelectors.name.value = "";
-  DOMSelectors.title.value = "";
-  DOMSelectors.image.value = "";
+    );
+  }
+  function erase() {
+    let removes = document.querySelectorAll(".remove");
+    removes.forEach((eachRemove) => {
+      eachRemove.addEventListener("click", (event) => {
+        event.target.parentElement.remove();
+      });
+    });
+  }
+
+  makeCard();
+  erase();
 });
-
-const resetButton = document.getElementById("reset-button");
-
-function addNewDiv() {}
-
-DOMSelectors.container.insertAdjacentHTML(
-  "afterbegin",
-  `<div class="display">
-        <image src></image>
-        <h1 class="song">Title: ${song}</h1>
-        <img src="${img}" class="myImage"alt="">
-        <h2 class="art">Song By:${artist}</h2>
-        <button id="remove"    </button>
-    </div>`
-);
